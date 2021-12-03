92329ec07b38db041a35ae2f7244a0f790f0da73
James Richardson <james.richardson@skybettingandgaming.com>
2021-12-03 08:08:07 +0000

    Day 3 code

---
+++ 3/day3.js
@@ -0,0 +1,64 @@
+import readArrayFromFile from "../utils/readArrayFromFile.js";
+
+
+const getEmptyBitNo = (len) => Array(len).fill(0);
+
+const convertStrToBin = (str) => {
+    const bin = getEmptyBitNo(str.length);
+
+    for(let i = 0; i < str.length; i++) {
+        bin[i] = +str.charAt(i);
+    }
+
+    return bin;
+}
+
+const getMostCommonBit = (val, total) => val >= total/2 ? 1 : 0;
+const getLeastCommonBit = (val, total) => val >= total/2 ? 0 : 1;
+
+const calcBinToDec = (val) => {
+    let bitVal = Math.pow(2, val.length-1);
+    let sum = 0;
+    
+    val.forEach((v) => {
+        sum += v * bitVal;
+        bitVal = bitVal / 2;
+    });
+
+    return sum;
+}
+
+const getRating = (data, binLen, getBit) => {
+    let filteredData = data;
+    let val = getEmptyBitNo(binLen);
+    
+    for(let i = 0; i < binLen; i++) {
+        let runningTotal = 0;
+
+        filteredData.forEach((bin) => runningTotal += bin[i]);
+
+        val[i] = getBit(runningTotal, filteredData.length);
+
+        filteredData = filteredData.filter(d => d[i] === val[i]);
+        
+        if(filteredData.length === 1) {
+            val = filteredData[0];
+            break;
+        }
+    }
+
+    return calcBinToDec(val);
+}
+
+const day3 = () => {
+    const filePath = `./3/input.txt`;
+    const data = readArrayFromFile(filePath).map(convertStrToBin);
+    const binLen = data[0].length;
+
+    const o2 = getRating(data, binLen, getMostCommonBit);
+    const co2 = getRating(data, binLen, getLeastCommonBit);
+
+    console.log(o2 * co2);
+}
+
+export default day3;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import day1 from "./1/day1.js";
 import day2 from "./2/day2.js";
+import day3 from "./3/day3.js";
 
 const newDay = (day) => console.log(`*** DAY ${day} ***`);
 const newLine = () => console.log("\n");
@@ -10,3 +11,7 @@ day1();
 newLine();
 newDay(2);
 day2();
+
+newLine();
+newDay(3);
+day3();
